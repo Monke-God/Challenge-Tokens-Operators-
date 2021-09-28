97d66f286ea977188ab922a704ae2b8328876920
Siddharth Singh <86530321+siddharth821@users.noreply.github.com>
2021-09-29 00:47:46 +0530

    Challenge : Tokens & Operators !
    PART 2

---
+++ challengeTokensOperators2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "Input two numbers" << endl;
+    cin >> a >> b;
+    cout << a * b << " is the product of " << a << " and " << b << endl;
+    return 0;
+}
